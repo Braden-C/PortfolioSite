276f80c71d52283ea419ae3dd75050894081542f
Braden Chapman <bradenchapman@Bradens-MacBook-Air.local>
2024-04-11 10:54:33 -0600

    add project sources to info context

--- src/Context/Info.js
+++ src/Context/Info.js
@@ -8,19 +8,21 @@ function Provider({ children }) {
       id: 1,
       title: "Favorite Books",
       desc: "App developed to track a users favorite books. Uses default and custom react hooks",
+      src: "https://github.com/Braden-C/Favorite-Books",
     },
     {
       id: 2,
       title: "Translation App",
       desc: "App developed to track a users favorite books. Uses default and custom react hooks",
+      src: "https://github.com/Braden-C/Translation-App",
     },
     {
       id: 3,
       title: "Score Keeper",
       desc: "App developed to track a users favorite books. Uses default and custom react hooks",
+      src: "https://github.com/Braden-C/ScoreKeeper",
     },
   ];
-  const Skills = ["React.JS", "JavaScript", "HTML", "CSS", "Git", "Npm"];
 
   const PersonalInfo = {
     firstName: "Braden",
@@ -30,6 +32,9 @@ function Provider({ children }) {
     bio: "Hello, I am a 27 year old Front-End developer based in central Colorado. I became interested in software/Web development at a very young age, My passion for technology and gaming has only grown over the years and lead me to pursue a career in tech.",
     skills: ["React", "JavaScript", "HTML", "CSS", "Git"],
   };
+
+  const Skills = PersonalInfo.skills;
+
   const fullName = `${PersonalInfo.firstName} ${PersonalInfo.lastName}`;
 
   const value = {
