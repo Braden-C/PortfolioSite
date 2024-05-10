7364a27f553b5401a3f41150f017581e2391e9ce
Braden Chapman <bradenchapman@Bradens-MacBook-Air.local>
2024-05-10 15:38:04 -0600

    add styling and import css file into contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,11 @@
 import useInfoContext from "../hooks/use-info-context";
+import "./Contact.css";
 
 function Contact() {
   const { PersonalInfo } = useInfoContext();
   return (
-    <div className="contact-me-container" id="Contact">
-      <div className="container-md text-center">
+    <div className="contact-me-container text-white fs-5" id="Contact">
+      <div className="container-md py-3 text-center">
         <div className="Header">
           <div className="horizontal-Header">
             <span>Contact Me</span>
@@ -27,31 +28,31 @@ function Contact() {
       </div>
       <div className="information-container">
         <ul className="list-group list-group-flush text-center">
-          <li className="list-group-item border-0 bg-transparent">
+          <li className="list-group-item border-0 text-white fs-5 py-2 bg-transparent">
             <a href="mailto:bradencchapman@yahoo.com" className="icon-link">
-              <i className="far fa-envelope" aria-hidden="true"></i>
+              <i className="far fa-envelope px-2" aria-hidden="true"></i>
             </a>
             <span>{PersonalInfo.email}</span>
           </li>
-          <li className="list-group-item border-0 bg-transparent">
+          <li className="list-group-item text-white fs-5 border-0 py-2 bg-transparent">
             <a
               href="https://www.linkedin.com/in/braden-chapman-b25a05234?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B%2Bvm36WcETd%2BVvXYIdiZdwQ%3D%3D"
               target="_blank"
               rel="noreferrer"
               className="icon-link"
             >
-              <i className="fab fa-linkedin-in" aria-hidden="true"></i>
+              <i className="fab fa-linkedin-in px-2" aria-hidden="true"></i>
             </a>
             <span>{PersonalInfo.linkedin}</span>
           </li>
-          <li className="list-group-item border-0 bg-transparent">
+          <li className="list-group-item text-white fs-5 border-0 py-2 bg-transparent">
             <a
               href="https://github.com/Braden-C"
               className="icon-link"
               target="_blank"
               rel="noreferrer"
             >
-              <i className="fab fa-github" aria-hidden="true"></i>
+              <i className="fab fa-github px-2" aria-hidden="true"></i>
             </a>
             <span>/Braden-C</span>
           </li>
